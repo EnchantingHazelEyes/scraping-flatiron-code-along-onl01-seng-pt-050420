7d9e8cb3ee48d519065886ed03365e7a5ccae877
Samantha Barros <samantha.barros95@gmail.com>
2020-07-04 05:05:56 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -5,6 +5,17 @@ require_relative './course.rb'
 
 class Scraper
   
+  
+  def get_page
+  
+  doc = 
+  
+  end
+  
+  
+  
+  
+  
   def print_courses
     self.make_courses
     Course.all.each do |course|
